cf9ee33bbde64f3b6de388e8b82633d0a2b57e83
Shayla Sexton <shaylae116@gmail.com>
2022-11-30 21:51:00 -0500

    add a comment to main on dev

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("this will soon become something!")
+
+# this will be the develop branch, were i test the code before i push.
+# i will create different branches if i want to 'branch' off smore!
